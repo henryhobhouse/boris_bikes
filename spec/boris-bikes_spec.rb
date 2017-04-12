2a5e8f1ecc077b3fd510067d2c41d10ea9173771
Bailey Kursar <bkursar@gmail.com>
2017-04-12 14:58:42 +0100

    Changed the rspec test in line with the walkthrough

--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -1,21 +1,17 @@
 require 'boris-bikes'
 
 describe DockingStation do
-  ## Testing the existance of release_bike
-  ## describe ".release_bike" do
-  ##  it { respond_to :release_bike }
-  ## end
+  ## The class 'DockingStation' should respond to the command to release a bike, using release_bike
+  it { is_expected.to respond_to :release_bike }
 
-  ## Writing a test for 2 things
-  describe "both getting the bike and asking if it works" do
-    test = Bike.new
-    it { expect test.working? }
+  it 'releases working bikes' do
+    ## (potential confusion) create a new variable 'bike' that is equal
+    ## to the return value of 'release_bike' method on the new instance of the
+    ## original class
+    ## e.g. bike = DockingStation.new.release_bike
+    bike = subject.release_bike
+    ## we should expect that the variable 'bike' should return 'true' when
+    ## we call the method 'be_working' on it
+    expect(bike).to be_working
   end
 end
-
-## describe Bike do
-  ## Testing whether or not a bike is working
-  ## describe ".working?" do
-    ## it { respond_to .working? }
-  ## end
-## end
